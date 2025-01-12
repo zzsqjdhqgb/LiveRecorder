7b91614f2d8416cb20385615dc286c59ea72e12f
zzsqjdhqgb <zzsqjdhqgb@outlook.com>
2025-01-12 15:35:26 +0800

    Add comments; Kill vlc subprocess after pressing Ctrl+C

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ import signal
 import psutil
 
 from config import VLC_PATH, OUTPUT_PATH
-
 from task import TASKS
 
+# 日志功能
 print_lock = threading.Lock()
 
-def thread_print(*args, **kwargs):
+
+def PrintLog(*args, **kwargs) -> None:
     with print_lock:
         print(*args, **kwargs)
 
 
+Stop_Flag = False
+flv_threads = []
+
+
 def Transform(input: str, output: str) -> None:
-    # input = os.path.abspath(input)
-    # output = os.path.abspath(output)
-    thread_print(f"Starting transforming {input} to {output}")
+    '''
+    由于 vlc 从直播中途开始录制，会导致时间戳错误
+    所以再次使用 vlc 转流
+    '''
+    PrintLog(f"Starting transforming {input} to {output}")
     subprocess.run([
         VLC_PATH,
         "--intf", "dummy",
@@ -30,20 +37,19 @@ def Transform(input: str, output: str) -> None:
         "--play-and-exit"
     ])
     os.remove(input)
-    thread_print(f"Transforming {input} to {output} finished")
-
-
-Stop_Flag = False
+    PrintLog(f"Transforming {input} to {output} finished")
 
 
 def Worker(url: str, name: str) -> None:
     while not Stop_Flag:
+        # 生成录制文件名
         cur_time = time.strftime('%Y-%m-%d_%H-%M-%S')
-        rec_out = f"{name}/tmp_{cur_time}.mp4"
-        rec_out = os.path.join(OUTPUT_PATH, rec_out)
-        # rec_out = os.path.abspath(rec_out)
-        thread_print(f"Recording to {rec_out}")
-        subprocess.run([
+        rec_out = os.path.join(OUTPUT_PATH, os.path.join(
+            name, f"/tmp_{cur_time}.mp4"))
+
+        # 开始录制
+        PrintLog(f"Recording to {rec_out}")
+        thr = subprocess.Popen([
             VLC_PATH,
             "--intf", "dummy",
             url,
@@ -51,42 +57,49 @@ def Worker(url: str, name: str) -> None:
             os.path.abspath(rec_out) + "}",
             "--play-and-exit"
         ])
-        thread_print(f"Recording {name} stopped")
-        rec_res = f"{
-            name}/{cur_time} - {time.strftime('%Y-%m-%d_%H-%M-%S')}.mp4"
-        rec_res = os.path.join(OUTPUT_PATH, rec_res)
+        flv_threads.append(thr)
+
+        # 录制结束
+        thr.wait()
+        flv_threads.remove(thr)
+        PrintLog(f"Recording {name} stopped")
+        # 生成输出文件名
+        rec_res = os.path.join(
+            OUTPUT_PATH, os.path.join(
+                name, f"/{cur_time} - {time.strftime('%Y-%m-%d_%H-%M-%S')}.mp4"))
+        # 重新编码文件
         threading.Thread(target=Transform, args=(rec_out, rec_res)).start()
-    thread_print(f"Worker {name} stopped")
+    PrintLog(f"Worker {name} stopped")
 
 
 def init() -> None:
+    # 创建输出目录
     if not os.path.exists(OUTPUT_PATH):
-        os.mkdir(OUTPUT_PATH)
+        os.makedirs(OUTPUT_PATH)
     for task in TASKS:
+        # 创建任务目录
         if not os.path.exists(os.path.join(OUTPUT_PATH, task["name"])):
-            os.mkdir(os.path.join(OUTPUT_PATH, task["name"]))
+            os.makedirs(os.path.join(OUTPUT_PATH, task["name"]))
         threading.Thread(target=Worker, args=(
             task["url"], task["name"])).start()
 
 
-def signal_handler(sig, frame) -> None:
+def Stop() -> None:
     global Stop_Flag
-    thread_print('Received Ctrl+C. Terminating all subprocesses...')
-    Stop_Flag = True
-    parent = psutil.Process()
-    for child in parent.children(recursive=True):
-        child.terminate()
-        try:
-            child.wait(timeout=10)  # 等待子进程结束，超时时间为10秒
-        except psutil.TimeoutExpired:
-            child.kill()  # 如果子进程没有在10秒内结束，强制终止
-    thread_print('All subprocesses terminated.')
-    sys.exit(0)
+    PrintLog('Stopping flvs...')
+    Stop_Flag=True
+    kill_lst=flv_threads[:]
+    for thr in kill_lst:
+        thr.kill()
+    for thr in kill_lst:
+        thr.wait()
+    PrintLog('All flv processes Stopped.')
 
 
 if __name__ == "__main__":
-    # 注册信号处理函数
-    signal.signal(signal.SIGINT, signal_handler)
     init()
-    while True:
-        time.sleep(1)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        Stop()
